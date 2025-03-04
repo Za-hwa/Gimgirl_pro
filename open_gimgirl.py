00e062a9653b8993cbd8b439931611fcda204ec0
Za-hwa <honeybee54321@naver.com>
2025-03-04 22:55:47 +0900

    Update open_gimgirl.py

--- open_gimgirl.py
+++ open_gimgirl.py
@@ -5,7 +5,7 @@ def main():
     st.header("2025 김해여고 펀딩 사이트")
     st.markdown("===")
 
-    col1,col2,col3= st.colums([1,1,1])
+    col1,col2,col3= st.columns([1,1,1])
 
     with col2:
         st.image('fund.png')
